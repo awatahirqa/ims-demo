cad7f0f57d16b861c7d8c27b86611d81425a5239
Waleed <WTahir@qa.com>
2021-01-14 17:29:28 +0000

    Updated Main and Test Scema to create relevent tables

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -2,3 +2,4 @@ create database if not exists ims;
 create table if not exists ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
 create table if not exists ims.item(id int primary key auto_increment, item_name varchar(40), price DECIMAL(10, 2));
 create table if not exists ims.orders(id int primary key auto_increment, CustomerID int, OrderLineID int);
+create table if not exists ims.orderline(id int primary key auto_increment, OrderID int, quantity int);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,3 +1,6 @@
 create database if not exists ims;
 drop table if exists ims.customers;
 create table ims.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table if not exists ims.item(id int primary key auto_increment, item_name varchar(40), price DECIMAL(10, 2));
+create table if not exists ims.orders(id int primary key auto_increment, CustomerID int, OrderLineID int);
+create table if not exists ims.orderline(id int primary key auto_increment, OrderID int, quantity int);
